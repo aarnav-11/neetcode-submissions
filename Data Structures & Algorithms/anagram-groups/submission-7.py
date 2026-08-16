58d674de8b85159ac97be2a109598060bddaed97
Aarnav Choudhary <aarnav11@g.ucla.edu>
2026-08-16 16:11:50 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #dict with sorted(word) : list(words) nlog n
+        #keys (tuple())
+        sortable = defaultdict(list)
+        for word in strs:
+            freq = [0]*26
+            for char in word:
+                i = ord(char) - ord("a")
+                freq[i] += 1
+            sortable[tuple(freq)].append(word)
+        ans = []
+        for values in sortable.values():
+            ans.append(values)
+        return ans
